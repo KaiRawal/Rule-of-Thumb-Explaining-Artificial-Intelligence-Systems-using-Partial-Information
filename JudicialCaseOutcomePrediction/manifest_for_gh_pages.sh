e2229e2f96abbbd9d276eba63133db4d097262c1
Kai Rawal <80235810+KaiRawal@users.noreply.github.com>
2025-11-20 15:48:32 +0000

    Github pages manifest via index.html

---
+++ JudicialCaseOutcomePrediction/manifest_for_gh_pages.sh
@@ -0,0 +1,20 @@
+
+
+curl -L http://localhost:8000/JudicialCaseOutcomePrediction/DATA/SPAN_EXPS_stride_35_128_1e-05/1_1_1/ -o ./DATA/SPAN_EXPS_stride_35_128_1e-05/1_1_1/index.html
+
+curl -L http://localhost:8000/JudicialCaseOutcomePrediction/DATA/SPAN_EXPS_stride_35_128_1e-05/1_1_0/ -o ./DATA/SPAN_EXPS_stride_35_128_1e-05/1_1_0/index.html
+
+curl -L http://localhost:8000/JudicialCaseOutcomePrediction/DATA/SPAN_EXPS_stride_35_128_1e-05/0_0_1/ -o ./DATA/SPAN_EXPS_stride_35_128_1e-05/0_0_1/index.html
+
+curl -L http://localhost:8000/JudicialCaseOutcomePrediction/DATA/SPAN_EXPS_stride_35_128_1e-05/0_0_0/ -o ./DATA/SPAN_EXPS_stride_35_128_1e-05/0_0_0/index.html
+
+curl -L http://localhost:8000/JudicialCaseOutcomePrediction/DATA/SHAP_MERGED_500/ -o ./DATA/SHAP_MERGED_500/index.html
+
+curl -L http://localhost:8000/JudicialCaseOutcomePrediction/DATA/LIME_MERGED_500/ -o ./DATA/LIME_MERGED_500/index.html
+
+curl -L http://localhost:8000/JudicialCaseOutcomePrediction/DATA/LIME_MERGED_5000/ -o ./DATA/LIME_MERGED_5000/index.html
+
+curl -L http://localhost:8000/JudicialCaseOutcomePrediction/DATA/IG_MERGED/ -o ./DATA/IG_MERGED/index.html
+
+curl -L http://localhost:8000/JudicialCaseOutcomePrediction/DATA/UNIFORM_SPANS/ -o ./DATA/UNIFORM_SPANS/index.html
+
