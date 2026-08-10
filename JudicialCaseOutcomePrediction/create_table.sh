6bc6be0d509020848dbd230cccff9036378574c1
Kai Rawal <80235810+KaiRawal@users.noreply.github.com>
2026-08-10 21:18:54 +0530

    RoT on Cases

--- JudicialCaseOutcomePrediction/create_table.sh
+++ JudicialCaseOutcomePrediction/create_table.sh
@@ -3,6 +3,6 @@ echo "index, comment, roberta_correct_only, case_count, AUC_weighted_case" > tab
 python3 ./Code/analyze_results_CSV.py --formula flip --directory SPAN_EXPS_stride_35_128_1e-05 --comment 'Token-level RoT 35' --csv >> 'table1.csv'
 python3 ./Code/analyze_results_CSV.py --formula flip --directory IG_MERGED --comment 'Integrated Gradients' --csv >> 'table1.csv'
 python3 ./Code/analyze_results_CSV.py --formula flip --directory SHAP_MERGED_500 --comment 'Default 500 Sample SHAP' --csv >> 'table1.csv'
-python3 ./Code/analyze_results_CSV.py --formula flip --directory LIME_MERGED_500 --comment 'Reduced 500 Sample LIME' --csv >> 'table1.csv'
+python3 ./Code/analyze_results_CSV.py --formula flip --directory LIME_MERGED_5000 --comment 'Default 5000 Sample LIME' --csv >> 'table1.csv'
 python3 ./Code/analyze_results_CSV.py --formula flip --directory UNIFORM_spans --comment 'Uniform Random Importance per PredEx span' --csv >> 'table1.csv'
 
